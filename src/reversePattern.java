82cb570970bcc1a52b024310a66e79d650825a55
dada <dadasahebkale.dk@gmail.com>
2022-12-31 20:05:28 +0530

    reverse pattern added

---
+++ src/reversePattern.java
@@ -0,0 +1,20 @@
+
+public class reversePattern {
+
+	public static void main(String args[])
+	{
+		int n=7;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j<n;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=(2*i-1);j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
